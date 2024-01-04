833fafac51941c05b2716b021ff5df9d78bab02e
Loic Yabili <loicyabili@gmail.com>
2024-01-04 18:58:40 +0200

    Draw the square container of the clock

--- lib/clock_page.dart
+++ lib/clock_page.dart
@@ -6,13 +6,18 @@ class ClockPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    double squareSize = MediaQuery.of(context).size.height / 1.8;
+
     return Scaffold(
       appBar: AppBar(title: const Text('Clock Page')),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        padding: const EdgeInsets.all(16),
-        child: const PaintClock(),
+      body: Center(
+        child: Container(
+          width: squareSize,
+          height: squareSize,
+          padding: const EdgeInsets.all(16),
+          child: const PaintClock(),
+        ),
       ),
     );
   }
@@ -38,7 +43,7 @@ class _PaintClockState extends State<PaintClock> {
   Widget build(BuildContext context) {
 
     return CustomPaint(
-      painter: ClockPainter(),
+      painter: ClockPainter(color: Theme.of(context).colorScheme.primary),
     );
 
   }
@@ -47,14 +52,57 @@ class _PaintClockState extends State<PaintClock> {
 
 class ClockPainter extends CustomPainter {
 
+  final Color color;
+
+  const ClockPainter({
+    this.color = Colors.blue,
+  });
+
   @override
   void paint(Canvas canvas, Size size) {
+
+    // a square container
+    final paint = Paint()
+      ..color = color
+      ..strokeWidth = 2
+      ..style = PaintingStyle.fill;
+
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
+
+    // add some style by drawing  circles
+    int lineIndex = 0, columnIndex = 0;
+    while (lineIndex < 5) {
+
+      while (columnIndex < 5) {
+        _drawStyleCircle(canvas:  canvas, size: const Size(100, 100),
+            lineIndex: lineIndex, columnIndex: columnIndex);
+        columnIndex++;
+      }
+      columnIndex = 0;
+      lineIndex++;
+    }
 
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
+  }
+
+  //draw a circle
+  void _drawStyleCircle({
+    required Canvas canvas,
+    required Size size,
+    required int lineIndex,
+    required int columnIndex}) {
+    final paint = Paint()
+      ..color = Colors.white.withOpacity(0.7)
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke;
+
+    Offset point = Offset(columnIndex * size.width, lineIndex * size.height);
+    canvas.drawCircle(point, size.width / 2, paint);
+
   }
 
 }
